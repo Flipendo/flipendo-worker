7bbc84a11952b35744b9b10c6f99da77ad341014
Sébastien Darses <sebastien@darses.fr>
2015-05-22 21:38:41 +1000

    worker config tweaks

--- worker.go
+++ worker.go
@@ -2,50 +2,40 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/streadway/amqp"
 )
 
-var config struct {
-	serverUrl         string
-	apiChannelName    string
-	workerChannelName string
-}
+const (
+	_apiChannelName    = "apiChannel"
+	_workerChannelName = "workerChannel"
+)
 
-var instance struct {
+var MQInstance struct {
 	server        *amqp.Connection
 	apiChannel    *amqp.Channel
 	workerChannel *amqp.Channel
 }
 
-func generateConfig() {
-	config.serverUrl = os.Getenv("RABBITMQ_PORT_5672_TCP_ADDR")
-	config.apiChannelName = "apiChannel"
-	config.workerChannelName = "workerChannel"
-}
-
 func failOnError(err error, msg string) {
 	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
 		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
 
 func connectToBroker() {
 	var err error
-	instance.server, err = amqp.Dial(config.serverUrl)
+	MQInstance.server, err = amqp.Dial(os.Getenv("RABBITMQ_PORT_5672_TCP_ADDR"))
 	failOnError(err, "Failed to connect to RabbitMQ")
 	fmt.Println("Successfully connected to RabbitMQ")
 }
 
 func disconnectFromBroker() {
-	instance.server.Close()
+	MQInstance.server.Close()
 }
 
 func main() {
-	generateConfig()
 	connectToBroker()
 	defer disconnectFromBroker()
 	testFile := NewFile("test.test")
