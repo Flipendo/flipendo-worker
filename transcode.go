dc77325640ef0ec8da4a145acb8eeb06335cedec
Sébastien Darses <sebastien@darses.fr>
2015-05-22 17:49:37 +1000

    added transcoding, no upload

---
+++ transcode.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var config struct {
+	baseCmd      string
+	srcFileName  string
+	videoCodec   string
+	audioCodec   string
+	experimental bool
+}
+
+func generateConfig(filename string) {
+	config.baseCmd = "ffmpeg"
+	config.srcFileName = filename
+	config.videoCodec = "h264"
+	config.audioCodec = "aac"
+	config.experimental = true
+}
+
+func getExecCmd() (string, []string) {
+	args := []string{}
+
+	// if config.overwrite {
+	// 	args = append(args, "-y")
+	// }
+	args = append(args, "-i")
+	args = append(args, config.srcFileName)
+	args = append(args, "-c:v")
+	args = append(args, config.videoCodec)
+	args = append(args, "-c:a")
+	// args = append(args, strconv.Itoa(config.chunkDuration))
+	// args = append(args, "-segment_list")
+	args = append(args, config.audioCodec)
+	// args = append(args, "-c")
+	// args = append(args, "copy")
+	if config.experimental {
+		args = append(args, "-strict")
+		args = append(args, "-2")
+	}
+	args = append(args, strings.Join([]string{"transcodedOutput",
+		".mkv"}, ""))
+	fmt.Println(args)
+
+	return config.baseCmd, args
+}
+
+func transcode() {
+	cmd, args := getExecCmd()
+	err := exec.Command(cmd, args...).Run()
+	if err != nil {
+		fmt.Println("failure in transcode")
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: [input filename]")
+		return
+	}
+	generateConfig(os.Args[1])
+	transcode()
+}
